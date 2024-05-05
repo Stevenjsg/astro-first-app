c2504721cfff6659428b1b5616e641f78dc1a1e9
Steven J. Silva <stevenibz-96@hotmail.com>
2024-05-05 22:04:21 +0200

    Add vercel integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321/',
   integrations: [tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics:{
+      enabled: true
+    }
+  })
 });
